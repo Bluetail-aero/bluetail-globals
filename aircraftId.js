602733227023ea18af6f8b03f23128713f0e0c50
Michael Zimmerman <mzimmerman0205@gmail.com>
2024-07-09 21:43:42 -0400

    This is way simpler to maintian, I'm dumb for not doing this.

--- aircraftId.js
+++ aircraftId.js
@@ -45,86 +45,54 @@ exports.isValidAircraftId = (aircraftId) => {
 exports.newAircraftId = (country_code, reg_n_number) =>
   ({ country_code, reg_n_number });
 
-const countryCodes = {
-  codes: [
-    'N', // United States
-    'VH-', // Australia
-    'OE-', // Austria
-    'OO-', // Belgium
-    'VP-B', 'VQ-B', // Bermuda
-    'PP-', 'PT-', 'PR-', 'PU-', 'PS-', // Brazil
-    'C-', // Canada
-    'B-', // China
-    'OY-', // Denmark
-    'F-', // France
-    'D-', // Germany
-    'SX-', // Greece
-    'VT-', // India
-    'EI-', 'EJ-', // Ireland
-    'I-', // Italy
-    'JA', 'JR', // Japan
-    'LX-', // Luxembourg
-    '9H-', // Malta
-    'XA-', 'XB-', 'XC-', // Mexico
-    'PH-', // Netherlands
-    'ZK-', 'ZL-', 'ZM-', // New Zealand
-    'RP-', // Philippines
-    'CS-', // Portugal
-    'RA-', 'RF-', // Russia
-    '9V-', // Singapore
-    'ZS-', 'ZT-', 'ZU-', // South Africa
-    'EC-', 'EM-', // Spain
-    'HB-', // Switzerland
-    'G-', // United Kingdom
-  // Sort country codes by length in descending order to handle subset clashes
-  ].sort((a, b) =>
-    b.length - a.length),
-  countryByCode: {
-    N: 'United States',
-    'VH-': 'Australia',
-    'OE-': 'Austria',
-    'OO-': 'Belgium',
-    'VQ-B': 'Bermuda',
-    'VP-B': 'Bermuda',
-    'PP-': 'Brazil',
-    'PT-': 'Brazil',
-    'PR-': 'Brazil',
-    'PU-': 'Brazil',
-    'PS-': 'Brazil',
-    'C-': 'Canada',
-    'B-': 'China',
-    'OY-': 'Denmark',
-    'F-': 'France',
-    'D-': 'Germany',
-    'SX-': 'Greece',
-    'VT-': 'India',
-    'EI-': 'Ireland',
-    'EJ-': 'Ireland',
-    'I-': 'Italy',
-    'JA, JR': 'Japan',
-    'LX-': 'Luxembourg',
-    '9H-': 'Malta',
-    'XA-': 'Mexico',
-    'XB-': 'Mexico',
-    'XC-': 'Mexico',
-    'PH-': 'Netherlands',
-    'ZK-': 'New Zealand',
-    'ZL-': 'New Zealand',
-    'ZM-': 'New Zealand',
-    'RP-': 'Philippines',
-    'CS-': 'Portugal',
-    'RA-': 'Russia',
-    'RF-': 'Russia',
-    '9V-': 'Singapore',
-    'ZS-': 'South Africa',
-    'ZT-': 'South Africa',
-    'ZU-': 'South Africa',
-    'EC-': 'Spain',
-    'EM-': 'Spain',
-    'HB-': 'Switzerland',
-    'G-': 'United Kingdom',
-  },
+const countryByCode = {
+  'N': 'United States',
+  'VH-': 'Australia',
+  'OE-': 'Austria',
+  'OO-': 'Belgium',
+  'VQ-B': 'Bermuda',
+  'VP-B': 'Bermuda',
+  'PP-': 'Brazil',
+  'PT-': 'Brazil',
+  'PR-': 'Brazil',
+  'PU-': 'Brazil',
+  'PS-': 'Brazil',
+  'C-': 'Canada',
+  'B-': 'China',
+  'OY-': 'Denmark',
+  'F-': 'France',
+  'D-': 'Germany',
+  'SX-': 'Greece',
+  'VT-': 'India',
+  'EI-': 'Ireland',
+  'EJ-': 'Ireland',
+  'I-': 'Italy',
+  'JA': 'Japan',
+  'JR': 'Japan',
+  'LX-': 'Luxembourg',
+  '9H-': 'Malta',
+  'XA-': 'Mexico',
+  'XB-': 'Mexico',
+  'XC-': 'Mexico',
+  'PH-': 'Netherlands',
+  'ZK-': 'New Zealand',
+  'ZL-': 'New Zealand',
+  'ZM-': 'New Zealand',
+  'RP-': 'Philippines',
+  'CS-': 'Portugal',
+  'RA-': 'Russia',
+  'RF-': 'Russia',
+  '9V-': 'Singapore',
+  'ZS-': 'South Africa',
+  'ZT-': 'South Africa',
+  'ZU-': 'South Africa',
+  'EC-': 'Spain',
+  'EM-': 'Spain',
+  'HB-': 'Switzerland',
+  'G-': 'United Kingdom',
 };
+
+const countryCodes = Object.keys(countryByCode).sort((a, b) => b.length - a.length);
 
 /**
  * Parse and validate a tailnumber, split it into country_code and reg_n_number and country
@@ -132,10 +100,10 @@ const countryCodes = {
  * @retuns {object} - { country_code, reg_n_number, region, fullTailNumber }; returns null if the country-code of hte tailnumber is not recognized.
  */
 exports.parseTailNumber = (tailnumber) => {
-  for (const code of countryCodes.codes) {
+  for (const code of countryCodes) {
     if (tailnumber.startsWith(code)) {
       return {
-        region: countryCodes.countryByCode[code],
+        region: countryByCode[code],
         country_code: code,
         reg_n_number: tailnumber.slice(code.length),
         fullTailNumber: tailnumber,

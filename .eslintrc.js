a60f6567f4aa734c6e83dbb91da4b6c680748caa
Jason Glover <jason@encode.co.nz>
2025-02-07 17:39:04 -0800

    Rearranging and preparing code for typescripting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,80 @@
 /* eslint-disable no-magic-numbers */
 module.exports = {
+  extends: ['airbnb-base', 'plugin:import/typescript'],
   env: {
-    browser: true,
-    commonjs: true,
+    node: true,
     es2021: true,
     mocha: true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
-    },
+  ignorePatterns: [
+    '**/dist/*',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'file-progress',
+    'import',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.ts'],
+      },
+      alias: {
+        map: [
+          ['@test', './test'], // This enables absolute imports from the test directory, just prefix the import with @test/
+        ],
+      },
+    },
   },
   rules: {
+    'newline-per-chained-call': ['off'],
+
+    'object-curly-newline': ['error', {
+      ImportDeclaration: { multiline: true, minProperties: 7 },
+      ObjectPattern: { multiline: true, minProperties: 5 },
+    }],
+
     'max-len': [2, {
       code: 200,
       tabWidth: 2,
       ignoreComments: true,
       ignoreUrls: true,
     }],
+
     // camcelcase is deeply embedded into our system already, this isnt useful
     camelcase: 'off',
+    'linebreak-style': 'off',
+
     'no-underscore-dangle': 'off',
+
     // Allow unsed vars from destructuring
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    'no-unused-vars': ['error', {
+      ignoreRestSiblings: true, destructuredArrayIgnorePattern: '^_',
+    }],
+
     // Allow for-of and for-in loops
     'no-restricted-syntax': [
       'error',
       'LabeledStatement',
       'WithStatement',
     ],
+
     'implicit-arrow-linebreak': ['error', 'below'],
-    'no-magic-numbers': ['warn', { enforceConst: true, detectObjects: true }],
+
+    // We prefer to not see magic numbers, but accept that some are inevitable, so this is just a warning
+    'no-magic-numbers': ['warn', {
+      enforceConst: true, detectObjects: true,
+    }],
+
     'no-await-in-loop': 'off',
   },
 };
